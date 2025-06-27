65d3c8bb5ec6ce190288001d82214c0db13f61d7
Wallacevr <wallacevr@gmail.com>
2025-06-27 15:03:36 -0300

    Publicação Nova

---
+++ HelpIn/publish/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script src="https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.8/countUp.min.js"></script>
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const options = { duration: 2, separator: '.' };
+
+        const counters = [
+            { id: 'counter-ongs', endVal: 200 },
+            { id: 'counter-voluntarios', endVal: 800 },
+            { id: 'counter-vagas', endVal: 1200 }
+        ];
+
+        counters.forEach(counter => {
+            const el = document.getElementById(counter.id);
+            if (el) {
+                const observer = new IntersectionObserver(entries => {
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            const countUp = new CountUp(counter.id, counter.endVal, options);
+                            countUp.start();
+                            observer.unobserve(entry.target);
+                        }
+                    });
+                });
+                observer.observe(el);
+            }
+        });
+    });
+
+
